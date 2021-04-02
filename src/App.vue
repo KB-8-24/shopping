02e4373a45ebf5f9d202be250437efee7d8eea65
yanjingsong <1559699429@qq.com>
2021-04-02 23:42:25 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {  },
+  directives: {  },
+  data() {
+    return {
+      
+    };
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
